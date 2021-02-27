eafa4893789df01a9b36c4f29fb4d31e15bd0624
mnzjadea <munoz.jade.a@gmail.com>
2021-02-27 19:29:50 +0800

    PLS

--- database/constants.php
+++ database/constants.php
@@ -3,15 +3,15 @@
 <?php
 
 //original
-define ('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'bookshaven');
+// define ('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', 'bookshaven');
 
 //remote sql
-// define ('DB_HOST', 'remotemysql.com');
-// define('DB_USER', 'MJhhpLhYIU');
-// define('DB_PASS', 'p5TCebHmJF');
-// define('DB_NAME', 'MJhhpLhYIU');
+define ('DB_HOST', 'remotemysql.com');
+define('DB_USER', 'MJhhpLhYIU');
+define('DB_PASS', 'p5TCebHmJF');
+define('DB_NAME', 'MJhhpLhYIU');
 
  ?>
